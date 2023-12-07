6c01a8087c0749d3dca60c8905d7dcac1b60a19d
DBWtv <dima.bwc@gmail.com>
2023-12-07 20:47:06 +0500

    modules

---
+++ static/js/add_column.js
@@ -0,0 +1,48 @@
+function getEnableDays(course_id) {
+    let id = course_id;
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/class/api/enable_days/', false);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    let data = 'course_id=' + encodeURIComponent(course_id) +
+        '&csrfmiddlewaretoken=' + encodeURIComponent(getCookie('csrftoken'));
+
+    xhr.send(data);
+
+    return JSON.parse(xhr.responseText);
+}
+
+function updateTable(day_id, course_id) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/class/api/update_table/', async = false);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+
+    let data = 'course_id=' + encodeURIComponent(course_id) +
+        '&csrfmiddlewaretoken=' + encodeURIComponent(getCookie('csrftoken')) +
+        '&day_id=' + encodeURIComponent(day_id);
+
+    xhr.send(data);
+
+    if (xhr.status != 201) {
+        throw new Error(xhr.status + ': ' + xhr.statusText);
+    }
+
+    return xhr.responseText;
+}
+
+function newCollonForm(days) {
+    let html = $('<div class="form-group"></div>')
+    let form = $('<form class="addColumnForm"></form>');
+    let select = $('<select id="daySelected" name="day">');
+    for (let i = 0; i < days.length; i++) {
+        let option = $('<option>');
+        option.attr('value', days[i].id);
+        option.text(days[i].name);
+        select.append(option);
+    }
+    let button = $('<button id="addColSubmit">add</button>');
+    form.append(select);
+    form.append(button);
+    html.append(form);
+    return html;
+}
--- static/js/add_row.js
+++ static/js/add_row.js
@@ -1,12 +1,12 @@
-function addRowRequest(week_id) {
-    var xhr = new XMLHttpRequest();
+function addRowRequest(week_id, course_id) {
+    let xhr = new XMLHttpRequest();
     xhr.open("POST", "/class/api/add_row/", false);  // Устанавливаем параметр false для синхронного режима
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
     // Формируем тело запроса
-    var data = 'week_id=' + encodeURIComponent(week_id) +
-            '&course_id=' + encodeURIComponent('{{ course.id }}') +
-            '&csrfmiddlewaretoken=' + encodeURIComponent('{{ csrf_token }}');
+    let data = 'week_id=' + encodeURIComponent(week_id) +
+            '&course_id=' + encodeURIComponent(course_id) +
+            '&csrfmiddlewaretoken=' + encodeURIComponent(getCookie('csrftoken'));
 
     xhr.send(data);
 
@@ -19,14 +19,19 @@ function addRowRequest(week_id) {
 
 function addRow(weekCell, numCols, table) {
     // Создаем новую строку
-    var newRow = $('<tr>');
+    let newRow = $('<tr>');
 
-    var cellHTML = $('#emptyCell').html();
+    let cellHTML = `
+        <button class="btn" id="addCell">
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 20 20">
+                <path fill="#888888" d="M10 1.6a8.4 8.4 0 1 0 0 16.8a8.4 8.4 0 0 0 0-16.8zm5 9.4h-4v4H9v-4H5V9h4V5h2v4h4v2z"></path>
+            </svg>
+        </button>`
 
     // Добавляем ячейки с данными
     for (var i = 1; i < numCols; i++) {
         if (i != 1) {
-            newRow.append('<td>' + cellHTML + '</td>');
+            newRow.append('<td id="emptyCell">' + cellHTML + '</td>');
         } else {
             newRow.append('<td class="calendarWeekCell">' + weekCell + '</td>');
         }
@@ -36,7 +41,7 @@ function addRow(weekCell, numCols, table) {
     table.append(newRow);
 
     // Копируем содержимое последней строки
-    var lastRowContent = $('#lastRow').html();
+    let lastRowContent = $('#lastRow').html();
 
     // Удаляем существующую кнопку
     $('#lastRow').remove();
@@ -44,24 +49,3 @@ function addRow(weekCell, numCols, table) {
     // Добавляем новую строку с содержимым последней строки
     table.append('<tr id="lastRow">' + lastRowContent + '</tr>');
 }
-
-$(document).ready(function () {
-    // Обработчик события нажатия кнопки
-    $('#schedule').on('click', '#addRow', function () {
-
-        // Выбираем таблицу и определяем количество столбцов
-        var table = $('#schedule tbody');
-        var numCols = $('#schedule thead th').length;
-
-        var weekCell = $('#schedule tbody .calendarWeekCell').length + 1;
-
-        try {
-            addRowRequest(weekCell)
-            addRow(weekCell, numCols, table);
-        }
-        catch (e) {
-            console.log(e);
-        }
-
-    });
-});

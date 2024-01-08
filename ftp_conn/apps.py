e66671657b00bbde098985a01a06da5773bdaa6c
DBWtv <dima.bwc@gmail.com>
2024-01-08 14:59:49 +0500

    delete not used

--- ftp_conn/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FtpConnConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ftp_conn'
--- ftp_conn/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-from . import views
-
-urlpatterns = [
-    re_path(r'^ftp_files/(?P<path>.*)$', views.get_file, name='ftp'),
-    re_path(r'^delete_file/(?P<path>.*)$', views.delete_file, name='ftp'),
-]
